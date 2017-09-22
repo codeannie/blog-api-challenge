a614edd5380c701df1c94a6bcae5de990aaa3ede
Annie Chiang <codepanduh@gmail.com>
2017-09-22 16:10:22 -0700

    added blogpost CRUD routes

--- blogPostRouter.js
+++ blogPostRouter.js
@@ -21,7 +21,57 @@ BlogPosts.create(
   { 
     id: uuid.v4(),
     title: 'Post 2',
-    content: 'This is the second post post',
+    content: 'This is the second post',
     author: 'Natsumi',
     publishDate: Date.now()
   });
+
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
+});
+
+router.post('/', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const blogPost = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+  res.status(201).json(blogPost);
+})
+
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
+  console.log(`Deleted blog post \`${req.params.id}\``);
+  res.status(204).end();
+});
+
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['title', 'content', 'author'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if(!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`;
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id (${req.body.id}) must match`);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating blog post \`${req.params.id}\``);
+  const updatedBlogPost = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+  });
+  res.status(204).end();
+});
+
+module.exports = router; 
