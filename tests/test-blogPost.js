4efb091fddf1e22572d65b83ea9c03d0f1aaf1ff
Annie Chiang <devbear22@gmail.com>
2017-10-03 21:59:07 -0700

    fixed syntax to address test errors

--- tests/test-blogPost.js
+++ tests/test-blogPost.js
@@ -33,59 +33,59 @@ describe('Blog API', function() {
           post.should.include.keys(expectedKeys);
         });
       });
-    }
+  });
 
-    //Test POST a new blog post
-    it('should post a new blog post on POST request', function() {
-      let newPost = {
-        title : 'Testing POST request',
-        content : 'I hope this works',
-        author : 'Joe Bruin',
-        publishDate : '10/03/2017'
-      };
-      return chai.request(app)
-        .post('/blog-posts')
-        .then(function(res) {
-          res.should.have.status(201);
-          res.body.should.be.a('object');
-          res.body.should.include.keys(expectedKeys);
-          res.body.id.should.not.be.null;
-          res.body.should.deep.equal(Object.assign(newPost, {id:res.body.id}));
-        });
-    });
+  //Test POST a new blog post
+  it('should post a new blog post on POST request', function() {
+    let newPost = {
+      title : 'Testing POST request',
+      content : 'I hope this works',
+      author : 'Joe Bruin',
+      publishDate : '10/03/2017'
+    };
+    return chai.request(app)
+      .post('/blog-posts')
+      .then(function(res) {
+        res.should.have.status(201);
+        res.body.should.be.a('object');
+        res.body.should.include.keys(expectedKeys);
+        res.body.id.should.not.be.null;
+        res.body.should.deep.equal(Object.assign(newPost, {id:res.body.id}));
+      });
+  });
 
-    //Test PUT on existing blog post 
-    it('should update an existing blog post on PUT request', function() {
-      const updatePost = {
-        title : 'We are updating with PUT!',
-        content : 'I hope this changed with PUT'
-      };
-      return chai.request(app)
-        .get('/blog-posts')
-        .then(function(res) {
-          updatePost.id = res.body[0].id;
-          return chai.request(app)
-            .put(`/blog-posts/${updatePost.id}`)
-            .send(updatePost);
-        })
-        .then(function(res) {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.should.be.a('object');
-          res.body.should.deep.equal(updatePost);
-        });
-    });  
+  //Test PUT on existing blog post 
+  it('should update an existing blog post on PUT request', function() {
+    const updatePost = {
+      title : 'We are updating with PUT!',
+      content : 'I hope this changed with PUT'
+    };
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        updatePost.id = res.body[0].id;
+        return chai.request(app)
+          .put(`/blog-posts/${updatePost.id}`)
+          .send(updatePost);
+      })
+      .then(function(res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.should.be.a('object');
+        res.body.should.deep.equal(updatePost);
+      });
+  });  
 
-    //Test DELETE an existing blog post 
-    it('should remove an existing blog post on DELETE request', function () {
-      return chai.request(app)
-        .get('/blog-posts')
-        .then(function(res) {
-          return chai.request(app)
-            .delete(`/blog-posts/${res.body[0].id}`);
-        })
-        .then(function(res) {
-          res.should.have.status(204);
-        });
-    });
+  //Test DELETE an existing blog post 
+  it('should remove an existing blog post on DELETE request', function () {
+    return chai.request(app)
+      .get('/blog-posts')
+      .then(function(res) {
+        return chai.request(app)
+          .delete(`/blog-posts/${res.body[0].id}`);
+      })
+      .then(function(res) {
+        res.should.have.status(204);
+      });
+  });
 });
