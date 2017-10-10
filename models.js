77b6cbcab0d7bcf16fe1d1b27585d1a973356ea2
Annie Chiang <devbear22@gmail.com>
2017-10-10 12:37:18 -0700

    added virtual property & instance method for author

--- models.js
+++ models.js
@@ -9,6 +9,20 @@ const blogPostSchema = mongoose.Schema ({
   created: {type: Date, default: Date.now},
 });
 
+//return author proprety as one string  (virtual property)
+blogPostSchema.virtual('author').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
+});
+
+blogPostSchema.methods.showAuthor = function() {
+  return {
+    id: this._id, 
+    author: this.author,
+    content. this.content,
+    title. this.title,
+    created. this.created
+  };
+}
+
 const blogPosts = mongoose.model(blogPosts, blogPostSchema);
-//should this be {blogPosts} ? 
-module.exports = blogPosts;
+module.exports = {blogPosts}; //?
